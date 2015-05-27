436153c3a976ecc7113d6c68189d061ca187195c
Nill Mefistophell <asklepiy@i.ua>
2015-05-27 23:17:20 +0300

    #avatar add

--- phpBB.php
+++ phpBB.php
@@ -138,6 +138,14 @@ class phpBB extends Component {
 
         return $this->_phpbb->user_change_email($phpbb_vars);
     }
+    
+    public function changeAvatar($user, $avatar) {
+        $phpbb_vars = is_int($user) ? array("user_id" => $user) : array("username" => $user);
+
+        $phpbb_vars['avatar'] = $avatar;
+
+        return $this->_phpbb->user_change_avatar($phpbb_vars);
+    }
 
     /**
      * Test if user is logged in phpBB
--- phpbbClass.php
+++ phpbbClass.php
@@ -258,11 +258,33 @@ class phpbbClass {
             if (!$phpbb_vars["user_id"] = $this->get_user_id_from_name($phpbb_vars["username"])) {
                 return $phpbb_result;
             }
-        } else {
-            echo 'x';
         }
 
         $db->sql_query("UPDATE " . USERS_TABLE . " SET user_email = '" . $phpbb_vars["email"] . "' WHERE user_id = '" . $phpbb_vars["user_id"] . "'");
+        $phpbb_result = "SUCCESS";
+
+        return $phpbb_result;
+    }
+
+    public function user_change_avatar($phpbb_vars) {
+        global $phpbb_root_path, $phpEx, $db, $config, $user, $auth, $cache, $template;
+        //fail presumtion
+        $phpbb_result = "FAIL";
+
+        //general info
+        $this->init(false);
+
+        //user functions
+        require_once($phpbb_root_path . "includes/functions_user." . $phpEx);
+
+        //get user_id if possible
+        if (!isset($phpbb_vars["user_id"])) {
+            if (!$phpbb_vars["user_id"] = $this->get_user_id_from_name($phpbb_vars["username"])) {
+                return $phpbb_result;
+            }
+        }
+
+        $db->sql_query("UPDATE " . USERS_TABLE . " SET user_avatar = '" . $phpbb_vars["avatar"] . "', user_avatar_type = 'avatar.driver.upload' WHERE user_id = '" . $phpbb_vars["user_id"] . "'");
         $phpbb_result = "SUCCESS";
 
         return $phpbb_result;
